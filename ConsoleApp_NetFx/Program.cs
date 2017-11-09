69246db12ca7ee7d891bfa1145c43f762a4a49b2
Immo Landwerth <immol@microsoft.com>
2017-11-09 15:45:44 -0800

    Initial commit

---
+++ ConsoleApp_NetFx/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Management;
+
+namespace ConsoleApp_NetFx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mo = new ManagementObject("Win32_Share.Name=\"C$\"");
+            mo.Get();
+
+            foreach (var p in mo.Properties)
+                Logger.Log($"{p.Name}={p.Value}");
+        }
+    }
+}
